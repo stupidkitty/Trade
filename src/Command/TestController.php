8b7332a2569af94216d62189fd27e19029ae3870
Alikdex <alikdex@gmail.com>
2018-12-25 21:20:03 +0300

    123

---
+++ src/Command/TestController.php
@@ -0,0 +1,36 @@
+<?php
+namespace SK\Module\TradeModule\Command;
+
+use Yii;
+use yii\console\Controller;
+use SK\Module\TradeModule\Out;
+
+
+class TestController extends Controller
+{
+    /**
+     * This command echoes what you have entered as the message.
+     * @param string $message the message to be echoed.
+     */
+    public function actionIndex()
+    {
+        set_time_limit(600);
+
+        $out = new Out;
+
+        $out->out();
+        /*$toTrade = 0;
+        $toSkim = 0;
+
+        for ($i = 0; $i < 100; $i++) {
+            if ($out->isSkim(50)) {
+                $toSkim ++;
+            } else {
+                $toTrade ++;
+            }
+        }*/
+
+        //dump("To trade: {$toTrade}", "To skim: {$toSkim}");
+    }
+
+}
--- src/Out.php
+++ src/Out.php
@@ -78,6 +78,7 @@ class Out
         exit;
     }
 
+    // /out.php?ps=eyJ1cmwiOiJodHRwOlwvXC90ZXN0LmNvbVwvMTIzLmh0bWwiLCJza2ltIjo5MH0=
     public function parseUri()
     {
         //$currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
