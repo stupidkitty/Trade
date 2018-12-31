9daae5b3ff433f9d76e8ba86e20893ba8a1fec25
Alikdex <alikdex@gmail.com>
2018-12-31 13:12:01 +0300

    Up to changes

--- src/Out.php
+++ src/Out.php
@@ -10,33 +10,42 @@ class Out
     private $nrSkim = 100; // noref skim
     private $firstClickSkim = 100; // first click skim
 
+    private $db;
 
+    public function __construct()
+    {
+        $this->db = new \PDO('mysql:dbname=rs_lc;host=127.0.0.1', 'test', 'testtest');
+    }
 
     public function run()
     {
-        $url = "http://test.com/";
         $getVars = $this->parseUri();
 
-
         // Если выпало на контент и урл для контента задан редиректим на контент.
-        if ($this->isSkim(90) && !empty($getVars['url'])) {
+        if ($this->isSkim(0) && !empty($getVars['url'])) {
             $this->redirect($getVars['url']);
         }
 
+        $trader = $this->getTrader();
+
         $tradeUrl = '';
         $redirectUrl = '';
-        if (empty($getVars['url']) && empty($tradeUrl)) {
+        if (empty($getVars['url']) && null === $trader) {
             // если оба пустые, отправляем на слив куда-то.
-            $redirectUrl = $defaultUrl;
-        } elseif (empty($tradeUrl)) {
+            $this->redirect($defaultUrl);
+        } elseif (null === $trader) {
             // если нету трейда, отправляем на контент.
-            $redirectUrl = $getVars['url'];
-        } elseif (empty($getVars['url'])) { // Ситуация когда нет трейдеров и нет урла на контент. Куда слать?!
-            // если нету урла в гетах, отправляем на трейд.
-            $redirectUrl = $tradeUrl;
+            $this->redirect($getVars['url']);
         }
 
-        $this->redirect($redirectUrl);
+        // дальше обрабатываем трейд.
+        if ($trader['forces_tally'] > 0) {
+            $this->decreaseTraderForcesTally($trader['trader_id']);
+        }
+
+        $this->sentTo($trader['trader_id']);
+
+        $this->redirect($trader['trader_url']);
     }
 
     /**
@@ -81,8 +90,6 @@ class Out
     // /out.php?ps=eyJ1cmwiOiJodHRwOlwvXC90ZXN0LmNvbVwvMTIzLmh0bWwiLCJza2ltIjo5MH0=
     public function parseUri()
     {
-        //$currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-
         $queryParams = [];
         if (!empty($_SERVER['QUERY_STRING'])) {
             \parse_str($_SERVER['QUERY_STRING'], $queryParams);
@@ -90,23 +97,72 @@ class Out
 
         if (isset($queryParams['ps'])) {
             $decodedJson = \base64_decode($queryParams['ps']);
-            //$jsonParams = \json_decode($base64Params, true);
-            //$queryParams = \array_merge($queryParams, $jsonParams);
-            $queryParams = \json_decode( $decodedJson, true);
-
-           // unset($queryParams['ps']);
+            $queryParams = \json_decode($decodedJson, true);
         }
 
-        /*$params = [
-            'url' => 'http://test.com/123.html',
-            'skim' => 90,
+        return  $queryParams;
+    }
+
+    /**
+     * Подбирает трейдера в таблице. Шлет только уникальные.
+     *
+     * @return array|null
+     */
+    private function getTrader()
+    {
+        $stmt = $this->db->prepare('
+            SELECT `t`.`trader_id`, `t`.`trade_url`, `t`.`forces_tally` 
+            FROM `traders` AS `t`
+            LEFT JOIN (
+                SELECT DISTINCT `trader_id`
+                FROM `taders_sent`
+                WHERE `ip_addr`=:ip_addr AND `created_at` > (NOW() - INTERVAL 1 MINUTE)
+            ) AS `ts` ON (`t`.`trader_id` = `ts`.`trader_id`)
+            WHERE `ts`.`trader_id` IS NULL AND `t`.`enabled` = 1
+            ORDER BY `forces_tally` DESC
+            LIMIT 1
+        ');
+        $stmt->execute([':ip_addr' => $this->getUserIp()]);
+
+        $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+        
+        if (empty($row)) {
+            return null;
+        }
+            
+        $trader_id = (int) $row['trader_id'];
+        $forces_tally = (int) $row['forces_tally'];
+        
+        $trader = [
+            'trader_id' => $trader_id,
+            'trade_url' => $row['trade_url'],
+            'forces_tally' => $forces_tally,
         ];
 
-        $json = \json_encode($params);
-        $base64 = \base64_encode($json);*/
+        return $trader;
+    }
 
+    private function decreaseTraderForcesTally($id)
+    {
+        $stmt = $this->db->prepare('
+            UPDATE `traders`
+               SET `forces_tally` = IF(`forces_tally` > 0, `forces_tally` - 1, 0)
+             WHERE `trader_id` = :trader_id
+        ');
+        $stmt->execute([':trader_id' => $id]);
+    }
 
-        //dump($_SERVER['REQUEST_URI'], $_SERVER, $queryParams, $base64);
-        return  $queryParams;
+    public function sentTo($id)
+    {
+        $stmt = $this->db->prepare('
+            INSERT INTO `taders_sent` (`trader_id`, `ip_addr`, `created_at`)
+            VALUES (:trader_id, :ip_addr, :created_at)
+        ');
+        $stmt->execute([':trader_id' => $id, ':ip_addr' => $this->getUserIp(), ':created_at' => \gmdate('Y-m-d H:i:s')]);
+    }
+
+    private function getUserIp()
+    {
+        return isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
     }
 }
